cab5d71597a1720513aa3c27ee4d4bde939bf7d7
abincleetus007 <133085039+Abincleetus007@users.noreply.github.com>
2024-12-27 23:54:09 +0530

    Update server.js

--- blog-backend/server.js
+++ blog-backend/server.js
@@ -7,12 +7,9 @@ const PORT=4003;
 
 
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Database Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error(err));
@@ -21,5 +18,4 @@ mongoose.connect(process.env.MONGO_URI)
 const postsRoutes=require("../blog-backend/routes/posts")
 app.use("/", postsRoutes);
 
-// const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
